92fbd23dcf829370e2dc0cc6b42c26eca21cfa22
Joao Wilbert <joaow@joaow-macbookpro2.roam.corp.google.com>
2013-11-13 17:22:30 +0000

    organised code

--- static/js/connect.js
+++ static/js/connect.js
@@ -1,79 +1,63 @@
+/*****************************************************
+/* VARS                                             */
+/****************************************************/
 
-// API STUFF
 var sscUrl = "http://sscweb.gsfc.nasa.gov/WS/sscr/2";
 var dataReqXml1 = '<?xml version="1.0" encoding="UTF-8" standalone="yes"?><DataRequest xmlns="http://sscweb.gsfc.nasa.gov/schema">'; // part 1 of a DataRequest
 var dataReqXml2 = '<BFieldModel><InternalBFieldModel>IGRF-10</InternalBFieldModel><ExternalBFieldModel xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:type="Tsyganenko89cBFieldModel"><KeyParameterValues>KP3_3_3</KeyParameterValues></ExternalBFieldModel><TraceStopAltitude>100</TraceStopAltitude></BFieldModel>'; // part 2 of a DataRequest
 var dataReqXml3 = '<OutputOptions><AllLocationFilters>true</AllLocationFilters><CoordinateOptions><CoordinateSystem>Gse</CoordinateSystem><Component>X</Component></CoordinateOptions><CoordinateOptions><CoordinateSystem>Gse</CoordinateSystem><Component>Y</Component></CoordinateOptions><CoordinateOptions><CoordinateSystem>Gse</CoordinateSystem><Component>Z</Component></CoordinateOptions><MinMaxPoints>2</MinMaxPoints></OutputOptions></DataRequest>'; // part 3 of a DataRequest
-
 var earthRadiusKm = 6378;  
 
-var plotList = [];
-var ONE_HOUR = 60 * 60 * 1000; /* ms */
-                                                                
-getObservatories();
+getSatellites();
 
-////////////////////// Get Locations ////////////////////// 
+/*****************************************************
+/* GET SATELLITE LIST                               */
+/****************************************************/ 
 
+function getSatellites() {
+    var xmlRequest = $.ajax({
+      type: 'GET',
+      url: sscUrl + '/observatories', 
+      dataType: 'json',
+      contentType: 'application/json',
+    });
 
-function plotSatellite(data) {
-    
-    // Plot Path
-    var material = new THREE.LineBasicMaterial({linewidth: 1, color: 0xffffff,opacity: 0.5});
-    var geometry = new THREE.Geometry();
-    var x,y,z;
-    
-    for(i=0; i < data.X.length; i++) {
-        
-        x = data.X[i]/earthRadiusKm * 1000;
-        y = data.Y[i]/earthRadiusKm * 1000;
-        z = data.Z[i]/earthRadiusKm * 1000;
-        //console.debug("X",x,"Y",y,"Z",z);
-        geometry.vertices.push(new THREE.Vector3(x, y, z));
-    }
-    
-    var line = new THREE.Line(geometry, material);
-    scene.add(line);   
-    
-    lineStart = new THREE.Vector3(data.X[0]/earthRadiusKm * 1000,data.Y[0]/earthRadiusKm * 1000,data.Z[0]/earthRadiusKm * 1000);
-    lineEnd = new THREE.Vector3(data.X[data.X.length-1]/earthRadiusKm * 1000,data.Y[data.Y.length-1]/earthRadiusKm * 1000,data.Z[data.Z.length-1]/earthRadiusKm * 1000);
-    
-    // Plot Start Point
-    partStartMaterial = new THREE.ParticleCanvasMaterial( { opacity: 0.5, color: 0xffffff, program: particleRender } );
-    particleStart = new THREE.Particle(partStartMaterial);
-    particleStart.position = lineStart;
-    particleStart.scale.x = particleStart.scale.y = particleStart.scale.z = 20;
-    scene.add(particleStart);
-    
-    // Plot End Point
-    partEndMaterial = new THREE.ParticleCanvasMaterial( { opacity: 0.5, color: 0xffffff, program: particleRender } );
-    particleEnd = new THREE.Particle(partEndMaterial);
-    particleEnd.position = lineEnd;
-    particleEnd.scale.x = particleEnd.scale.y = particleEnd.scale.z = 20;
-    scene.add(particleEnd);
-    
+    xmlRequest.done( handleObservatories );    
 }
 
-function drawAsParticles(data) {
-    var geometry = new THREE.Geometry();
-    var x,y,z;
-    for ( i = 0; i < data.X.length; i ++ ) {
-
-        x = data.X[i]/earthRadiusKm * 1000;
-        y = data.Y[i]/earthRadiusKm * 1000;
-        z = data.Z[i]/earthRadiusKm * 1000;
-        geometry.vertices.push(new THREE.Vector3(x, y, z));
-        console.debug(x,y,z)
-    }
-    material = new THREE.ParticleBasicMaterial({ color: 0xffffff, size: 4 });
-   
-    particles = new THREE.ParticleSystem( geometry, material );
-    scene.add( particles );
-    				
+function handleObservatories(data) {
+    var elId;
+     
+    console.debug("Could fetch:",data.Observatory.length,"satellites");
+    console.debug("Sats are:",data);
+    
+    // Get One Satellite
+    var testEl = data.Observatory[3].Id;  
+    startTime = removeOneHour(data.Observatory[3].EndTime); // remove one hour
+    endTime = data.Observatory[3].EndTime;
+    getLocation(testEl,startTime,endTime); // new API call
+    
+    /*
+    // Get all satellites
+    $.each(data.Observatory, function(index,value) {
+        elId = data.Observatory[index].Id;   
+             
+        startTime = removeOneHour(data.Observatory[index].EndTime); // remove one hour
+        endTime = data.Observatory[index].EndTime;
+        //console.debug(elId);
+        getLocation(elId,startTime,endTime); // new API call
+    });
+    */
 }
+
+
+/*****************************************************
+/* GET SATELLITE LOCATIONS                          */
+/****************************************************/
 
 function getLocation(id,startTime,endTime) {
     
-    var timeReqXml = '<TimeInterval><Start>' + startTime +'</Start><End>' + endTime + '</End></TimeInterval>';     // Time format: 1997-08-26T00:00:00.000Z
+    var timeReqXml = '<TimeInterval><Start>' + startTime +'</Start><End>' + endTime + '</End></TimeInterval>';  // Time format: 1997-08-26T00:00:00.000Z
     var satReqXml = '<Satellites><Id>' + id + '</Id>' + '<ResolutionFactor>2</ResolutionFactor></Satellites>';
     var request = dataReqXml1 + timeReqXml + dataReqXml2 + satReqXml + dataReqXml3;    
     
@@ -88,63 +72,23 @@ function getLocation(id,startTime,endTime) {
         processData: false,
         success: handleLocations, 
     }); 
-    
-    
 }
+
 function handleLocations(data) {
     
-    if(data.Result.StatusCode == "Success") {
+    if(data.Result.StatusCode == "Success") {  
         console.debug("SUCCESS Satellite:",data.Result.Data.Id, " ",data.Result);     // If can get current locations store in array
-        //plotSatellite(data.Result.Data.Coordinates);
-        drawAsParticles(data.Result.Data.Coordinates);
-        
+        plotSatellite(data.Result.Data.Coordinates);
+        //drawAsParticles(data.Result.Data.Coordinates);
     } else {
         console.debug("FAILED: Satellite:",data.Result.StatusCode,data.Result.StatusSubCode);
     }
-    
 }
 
+/*****************************************************
+/* HELPERS                                          */
+/****************************************************/
 
-////////////////////// Get Satellites ////////////////////// 
-
-function getObservatories() {
-    var xmlRequest = $.ajax({
-      type: 'GET',
-      url: sscUrl + '/observatories', 
-      dataType: 'json',
-      contentType: 'application/json',
-    });
-
-    xmlRequest.done( handleObservatories );    
-}
-
-function handleObservatories(data) {
-    var elStartTime = getDateTime().now;
-    var elEndTime = getDateTime().later;
-    var elId;
-     
-    
-    console.debug("Could fetch:",data.Observatory.length,"satellites");
-    console.debug("Sats are:",data);
-    
-    // Get all satellites
-    $.each(data.Observatory, function(index,value) {
-        
-        
-        
-        elId = data.Observatory[index].Id;        
-        startTime = removeOneHour(data.Observatory[index].EndTime); // remove one hour
-        endTime = data.Observatory[index].EndTime;
-        getLocation(elId,startTime,endTime); // new API call
-    });
-    
-    
-    
-    
-}
-
-
-////////////////////// Get Locations //////////////////////
 Date.prototype.removeHours= function(h){
     this.setHours(this.getHours()-h);
     return this;
@@ -153,40 +97,6 @@ Date.prototype.removeHours= function(h){
 function removeOneHour(str) {
     // "2013-11-03T22:00:00.000Z"
     str = str.substring(0, str.length - 5);
-    var date = new Date(str).removeHours(24);
+    var date = new Date(str).removeHours(1);
     return date.toISOString();
 }
-
-
-
-function getDateTime() {
-    var now     = new Date(); 
-    var year    = now.getFullYear();
-    var month   = now.getMonth()+1; 
-    var day     = now.getDate();
-    var hour    = now.getHours();
-    var minute  = now.getMinutes();
-    var second  = now.getSeconds(); 
-    if(month.toString().length == 1) {
-        var month = '0'+month;
-    }
-    if(day.toString().length == 1) {
-        var day = '0'+day;
-    }   
-    if(hour.toString().length == 1) {
-        var hour = '0'+hour;
-    }
-    if(minute.toString().length == 1) {
-        var minute = '0'+minute;
-    }
-    if(second.toString().length == 1) {
-        var second = '0'+second;
-    }   
-    var now = year+'-'+month+'-'+day+'T'+hour+':'+minute+':'+second+".000Z";   
-    var later = year+'-'+month+'-'+day+'T'+(hour+1)+':'+minute+':'+second+".000Z";   
-    
-    // One hour later
-    return { "now" : now, "later" : later };
-}
-
-
--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,16 +1,24 @@
-var POS_X = 1800;
-var POS_Y = 500;
-var POS_Z = 2000;
+/*****************************************************
+/* GLOBAL                                           */
+/****************************************************/
 
+var CAM_POS_X = 1800;
+var CAM_POS_Y = 500;
+var CAM_POS_Z = 2000;
 var globeGroup;
 
 init();
 animate();
 
+//http://jsfiddle.net/crossphire/DAktM/
+
+/*****************************************************
+/* ENGINE                                           */
+/****************************************************/
 
 function init() {
   camera = new THREE.PerspectiveCamera( 45  , window.innerWidth / window.innerHeight,1,6000 );
-  camera.position.set(POS_X,POS_Y, POS_Z);
+  camera.position.set(CAM_POS_X,CAM_POS_Y, CAM_POS_Z);
   camera.lookAt(new THREE.Vector3(0,0,0));
   globeGroup = new THREE.Object3D();
   
@@ -20,10 +28,9 @@ function init() {
   
   addEarth();
   addClouds();
-  addStructure();
   
-  //renderer = new THREE.CanvasRenderer();
-  renderer = new THREE.WebGLRenderer();
+  renderer = new THREE.CanvasRenderer();
+  //renderer = new THREE.WebGLRenderer();
   
   renderer.setSize( window.innerWidth, window.innerHeight);
   renderer.setClearColorHex(0x111111);
@@ -31,36 +38,21 @@ function init() {
   container.appendChild( renderer.domElement );
 }
 
-function addStructure() {
-    
-    
-    var nodeGroup = new THREE.Object3D();
-    nodeGeometry = new THREE.IcosahedronGeometry(900,2);
-    
-    nodeMaterial = new THREE.MeshBasicMaterial({ linewidth: 2, opacity:0.05, color : 0xfffff, wireframe : true });
-    nodeMesh = new THREE.Mesh(nodeGeometry, nodeMaterial);			
-    //nodeMesh.position = position;
-    //console.debug(nodeMesh.geometry.faces);
-
-    var faces = nodeMesh.geometry.vertices;
-    for(var i=0; i < faces.length; i++) {
-        //console.debug(faces[i]);
-        
-        
-        material = new THREE.ParticleCanvasMaterial( { opacity: 0.5, color: 0xffffff, program: particleRender } );
-        particle = new THREE.Particle(material);
-        particle.position = new THREE.Vector3(faces[i].x,faces[i].y,faces[i].z);
-        //globeGroup.add(particle);
-        particle.scale.x = particle.scale.y = particle.scale.z = 3;
-        
-        //faces[i].color.r = 0xb10707;
-        //faces[i].color = "#"+((1<<24)*Math.random()|0).toString(16);
-    }
-    
-    //scene.add(nodeMesh);
-    
+function animate() {
+    var timer = Date.now() * 0.0001;
+    //animateSat();
+    camera.position.x = (Math.cos( timer ) *  3900);
+    camera.position.z = (Math.sin( timer ) * 3900) ;
+    camera.lookAt( scene.position );
+    renderer.render( scene, camera );    
+    requestAnimationFrame( animate );
 }
-// add the earth
+
+/*****************************************************
+/* WORLD                                            */
+/****************************************************/
+
+// Earth
 function addEarth() {
    var spGeo = new THREE.SphereGeometry(800,50,50);
    var planetTexture = THREE.ImageUtils.loadTexture( "imgs/map3.jpg" );
@@ -71,7 +63,7 @@ function addEarth() {
    globeGroup.add(sp);
 }
 
-// add clouds
+// Clouds
 function addClouds() {
     var spGeo = new THREE.SphereGeometry(800,50,50);
     var cloudsTexture = THREE.ImageUtils.loadTexture( "imgs/clouds.jpg" );
@@ -82,105 +74,79 @@ function addClouds() {
     globeGroup.add( meshClouds );
 }
 
-// add a simple light
+// Light
 function addLights() {
    light = new THREE.DirectionalLight(0x3333ee, 3.5, 500 );
    scene.add( light );
    light.position.set(POS_X,POS_Y,POS_Z);
 }
 
-function animate() {
-    var timer = Date.now() * 0.0001;
-    
-    camera.position.x = (Math.cos( timer ) *  3900);
-    camera.position.z = (Math.sin( timer ) * 3900) ;
-    camera.lookAt( scene.position );
-    renderer.render( scene, camera );    
-    requestAnimationFrame( animate );
-}
-
-
-function latLongToVector3(lat, lon, radius, heigth) {
-    var phi = (lat)*Math.PI/180;
-    var theta = (lon-180)*Math.PI/180;
-
-    var x = -(radius+heigth) * Math.cos(phi) * Math.cos(theta);
-    var y = (radius+heigth) * Math.sin(phi);
-    var z = (radius+heigth) * Math.cos(phi) * Math.sin(theta);
-
-    return new THREE.Vector3(x,y,z);
-}
-
+// Particle
 function particleRender( context ) {
 	context.beginPath();
 	context.arc( 0, 0, 1, 0,  Math.PI * 2, true );
 	context.fill();
 }
 
-function mapPoint(lat,lon) {
-    material = new THREE.ParticleCanvasMaterial( { color: 0xffffff, program: particleRender } );
-    particle = new THREE.Particle(material);
-    var pos = latLongToVector3(lat,lon,805,2);
-    particle.position = pos;
-    particle.scale.x = particle.scale.y = particle.scale.z = 9;
-    //globeGroup.add(particle);
-    return pos;
-}
+/*****************************************************
+/* SAT                                              */
+/****************************************************/
 
-function createConnection(p1,p2) {
-    // smooth my curve over this many points
-    var numPoints = 100;
-
-    var midx = (p1.x + p2.x);
-    var midy = (p1.y + p2.y);
+function plotSatellite(data) {
     
-    
-    spline = new THREE.SplineCurve3([
-       p1,
-       new THREE.Vector3(midx, midy, 500),
-       p2
-    ]);
-
-    var material = new THREE.LineBasicMaterial({
-        color: 0x00ae6f, opacity: 1, linewidth: 3
-    });
-
+    // Plot Path
+    var material = new THREE.LineBasicMaterial({linewidth: 1, color: 0x70c2c7,opacity: 0.5});
     var geometry = new THREE.Geometry();
-    var splinePoints = spline.getPoints(numPoints);
-
-    for(var i = 0; i < splinePoints.length; i++){
-        geometry.vertices.push(splinePoints[i]);  
+    var x,y,z;
+    
+    for(i=0; i < data.X.length; i++) {
+        
+        x = data.X[i]/earthRadiusKm * 1000;
+        y = data.Y[i]/earthRadiusKm * 1000;
+        z = data.Z[i]/earthRadiusKm * 1000;
+        //console.debug("X",x,"Y",y,"Z",z);
+        geometry.vertices.push(new THREE.Vector3(x, y, z));
     }
-
+    
     var line = new THREE.Line(geometry, material);
-   
-    //globeGroup.add(line);
+    scene.add(line);   
+    
+    //scaleObject(line,0,1,1000);
+    lineStart = new THREE.Vector3(data.X[0]/earthRadiusKm * 1000,data.Y[0]/earthRadiusKm * 1000,data.Z[0]/earthRadiusKm * 1000);
+    lineEnd = new THREE.Vector3(data.X[data.X.length-1]/earthRadiusKm * 1000,data.Y[data.Y.length-1]/earthRadiusKm * 1000,data.Z[data.Z.length-1]/earthRadiusKm * 1000);
+    
+    // Plot Start Point
+    partStartMaterial = new THREE.ParticleCanvasMaterial( { opacity: 0.5, color: 0xffffff, program: particleRender } );
+    particleStart = new THREE.Particle(partStartMaterial);
+    //particleStart.position = lineStart;
+    particleStart.scale.x = particleStart.scale.y = particleStart.scale.z = 20;
+    scene.add(particleStart);
+    
+    // Plot End Point
+    partEndMaterial = new THREE.ParticleCanvasMaterial( { opacity: 0.5, color: 0xffffff, program: particleRender } );
+    particleEnd = new THREE.Particle(partEndMaterial);
+    particleEnd.position = lineEnd;
+    particleEnd.scale.x = particleEnd.scale.y = particleEnd.scale.z = 20;
+    scene.add(particleEnd);
     
 }
 
-function pointPair(p1,p2) {
-    var point1 = new mapPoint(p1.lat,p1.lon);
-    var point2 = new mapPoint(p2.lat,p2.lon);
-    //createConnection(point1,point2);
+function drawAsParticles(data) {
+    var geometry = new THREE.Geometry();
+    var x,y,z;
+    
+    for (i = 0; i < data.X.length; i ++) {
+        x = data.X[i]/earthRadiusKm * 1000;
+        y = data.Y[i]/earthRadiusKm * 1000;
+        z = data.Z[i]/earthRadiusKm * 1000;
+        geometry.vertices.push(new THREE.Vector3(x, y, z));
+        //console.debug(x,y,z)
+    }
+    
+    material = new THREE.ParticleBasicMaterial({ color: 0xffffff, size: 4 });
+    particles = new THREE.ParticleSystem( geometry, material );
+    scene.add( particles );
+    				
 }
 
-/*
-pointPair(
-    { 
-        lat: -19.9597163, 
-        lon: -43.9307278 
-    },
-    {   lat: 24.84656534821976, 
-        lon: -78.22265625 
-});
-*/
-
-pointPair(
-    { 
-        lat: 20.000000, 
-        lon: -10.000000 
-    },
-    {   lat: 51.509426, 
-        lon: -0.10437 
-});
 
